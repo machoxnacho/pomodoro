263d0d1ccccb43031a9bcd2e3dc4d4febcb4b90b
machoxnacho <bngoc.ptran@gmail.com>
2025-08-04 17:46:03 -0400

    Update testing.txt

--- backend/testing.tsx
+++ backend/testing.tsx
@@ -1,3 +1,3 @@
 aws ecr get-login-password --region YOUR_REGION | docker login --username AWS --password-stdin YOUR_AWS_ID.dkr.ecr.YOUR_REGION.amazonaws.com
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 900546069136.dkr.ecr.YOUR_REGION.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 900546069136.dkr.ecr.us-east-1.amazonaws.com
