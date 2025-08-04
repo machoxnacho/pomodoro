a6ff170e438955f66a9f6f9170bb3147396e5ee9
machoxnacho <bngoc.ptran@gmail.com>
2025-08-04 16:56:04 -0400

    Update testing.tsx

--- backend/testing.tsx
+++ backend/testing.tsx
@@ -1,35 +1,14 @@
-<div className="home-controls">
-            <div className="coin-display">
-              <img
-              src={coinIcon}
-              alt="Coin"
-              className="coin-icon"
-              onClick={() => setShowShopMenu(true)}
-              style={{ cursor: 'pointer' }}
-              />
-              <span className="coint-count">{coins}</span>
-            </div>
-            <div>
-            <button className='image-button' onClick={() => setShowCollectionMenu(true)}>
-              <img src={menuIcon} alt="Menu" className='menu-icon' />
-            </button>
-            </div>
-          </div>
+# Use the official base image
+FROM rockylinux:8
 
-.image-button {
-  display: flex;
-  align-items: center;
-  gap: 6px;
-  position: absolute;
-  left: 1px; 
-  top: 4px;
-  font-weight: bold;
-  font-size: 1.2rem;
-  background: transparent;
-  border: none;
-}
+# Install the Apache HTTP server package from the CentOS repository
+RUN yum install httpd -y
 
-.image-button:hover {
-  transform: scale(1.1);
-  cursor: pointer;
-}
+# Copy the index.html file from the Docker build context to the default Apache document root directory in the container
+COPY index.html /var/www/html/
+
+# Specify the command to run when the container starts, which starts the Apache HTTP server in the foreground
+CMD ["/usr/sbin/httpd","-D","FOREGROUND"]
+
+# Expose port 80 to allow incoming HTTP traffic to the container
+EXPOSE 80
