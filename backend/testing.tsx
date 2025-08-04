71ea44abf5064ad2b8246ae80e146fc55c322469
machoxnacho <bngoc.ptran@gmail.com>
2025-08-04 18:00:29 -0400

    Update testing.tsx

--- backend/testing.tsx
+++ backend/testing.tsx
@@ -1,25 +1,19 @@
-docker tag smiski-backend:latest 900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend/smiski-backend:latest
-
-docker push 900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend/smiski-backend:latest
-
-docker push 900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend/smiski-backend:latest
-The push refers to repository [900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend/smiski-backend]
-f5f9d435c580: Waiting 
-280bbe393e78: Waiting 
-e7fb18dd4198: Waiting 
-28b4958639f9: Waiting 
-d757d01e4983: Waiting 
-4197994b707b: Waiting 
-1f4f297e4f69: Waiting 
-b58ee5cb7152: Waiting 
-43da18ac37b7: Waiting 
-6cba32d1c1ee: Waiting 
+docker push 900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend:latest
+The push refers to repository [900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend]
 4c4eb3d16508: Waiting 
+4197994b707b: Waiting 
+d757d01e4983: Waiting 
+43da18ac37b7: Waiting 
+280bbe393e78: Waiting 
+b58ee5cb7152: Waiting 
 354ffb7f5514: Waiting 
+28b4958639f9: Waiting 
+f5f9d435c580: Waiting 
+e7fb18dd4198: Waiting 
 1a12b4ea7c0c: Waiting 
-unknown: unexpected status from HEAD request to https://900546069136.dkr.ecr.us-east-1.amazonaws.com/v2/smiski-backend/smiski-backend/blobs/sha256:1f4f297e4f699ae0f384d5cc1ea42065f58a115aa0a634d427cbb186f91cb4d0: 403 Forbidden
-
-
+6cba32d1c1ee: Waiting 
+1f4f297e4f69: Waiting 
+unknown: unexpected status from HEAD request to https://900546069136.dkr.ecr.us-east-1.amazonaws.com/v2/smiski-backend/blobs/sha256:b58ee5cb7152015437e4a9b3066ae9e25a26a3bef6617d0b7f25368511c2d954: 403 Forbidden
 
 docker tag smiski-backend:latest 900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend:latest
 docker push 900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend:latest
