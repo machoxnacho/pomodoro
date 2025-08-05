8b7ed9ef47eda932dfb3983f244e2ef065647996
machoxnacho <bngoc.ptran@gmail.com>
2025-08-05 16:00:41 -0400

    Update testing.tsx

--- backend/testing.tsx
+++ backend/testing.tsx
@@ -5,3 +5,8 @@ docker buildx build \
   -t 900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend:latest \
   --push .
 
+    900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend
+
+ERROR: failed to build: failed to solve: failed to push 900546069136.dkr.ecr.us-east-1.amazonaws.com/smiski-backend:latest: unexpected status from HEAD request to https://900546069136.dkr.ecr.us-east-1.amazonaws.com/v2/smiski-backend/blobs/sha256:37927ed901b1b2608b72796c6881bf645480268eca4ac9a37b9219e050bb4d84: 403 Forbidden
+
+View build details: docker-desktop://dashboard/build/relaxed_diffie/relaxed_diffie0/ruf9w58amn4h617tlgwm4da1
